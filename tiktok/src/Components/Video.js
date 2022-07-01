45999c724f672d1dd33a0568c58dcedc6093075e
Duy Nguyen <duytrong.nguyen77@gmail.com>
2022-07-01 16:43:52 -0400

    Initial

---
+++ tiktok/src/Components/Video.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Video.css";
+
+function Video() {
+	return (
+		<div className="video">
+			<video src="https://p16-sign.tiktokcdn-us.com/obj/tos-useast5-p-0068-tx/abbf93affeb54c4f9337fa082a4c6228_1651638790?x-expires=1656727200&x-signature=kSEqux2%2FjPmTmnwPTnhYY2aoiJY%3D"></video>
+			{/* VideoFooter */}
+			{/* VideoSidebar */}
+		</div>
+	);
+}
+
+export default Video;
