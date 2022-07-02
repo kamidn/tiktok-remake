bf03ebceed90236a9b7fc37de1562a058d6bd02e
Duy Nguyen <duytrong.nguyen77@gmail.com>
2022-07-02 15:21:53 -0400

    Commit

--- tiktok/src/App.js
+++ tiktok/src/App.js
@@ -1,16 +1,23 @@
-import React from "react";
-import "./App.css";
-import Video from "./Components/Video.js";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <div className="app">
-			<div className="app__videos">
-				<Video />
-				<Video />
-				<Video />
-				<Video />
-			</div>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
