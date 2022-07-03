658558c01317a7b5928c88cc5a0cfb1b70f1198e
Duy Nguyen <duytrong.nguyen77@gmail.com>
2022-07-03 19:58:47 -0400

    Commit

--- tiktok/src/App.js
+++ tiktok/src/App.js
@@ -1,29 +1,36 @@
-import React from "react";
+import axios from "./axios.js";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Video from "./Components/Video.js";
 
 function App() {
+	const [videos, setVideos] = useState([]);
+
+	useEffect(() => {
+		async function fetchPosts() {
+			const response = await axios.get("/v2/posts");
+			setVideos(response.data);
+
+			return requestAnimationFrame;
+		}
+
+		fetchPosts();
+	}, []);
+
 	return (
 		<div className="app">
 			<div className="app__videos">
-				<Video
-					url="https://v16m-webapp.tiktokcdn-us.com/abf5c1d995f5df944701cbe87048f9d7/62c224c2/video/tos/useast2a/tos-useast2a-ve-0068c003/c89bd021836547f2a2c76f3923eba2d1/?a=1988&ch=0&cr=0&dr=0&lr=tiktok_m&cd=0%7C0%7C1%7C0&cv=1&br=1224&bt=612&cs=0&ds=3&ft=ebtHKH-qMyq8ZkrZ6we2NE0yfl7Gb&mime_type=video_mp4&qs=0&rc=Mzc1ZjRpNjo3OjU7PDg8NUBpam5zPDo6ZnZmPDMzNzczM0AxLzYuYDQyXjQxNmFeM2EvYSNvZGVlcjRvaF9gLS1kMTZzcw%3D%3D&l=2022070317223301000400500600304700444D35"
-					channel="Channel1"
-					description="This is description"
-					song="This is song"
-					likes={123}
-					messages={234}
-					shares={345}
-				/>
-				<Video
-					url="https://v16m-webapp.tiktokcdn-us.com/a013b5078801f027d327435f5396521e/62c22f50/video/tos/useast2a/tos-useast2a-pve-0068/7d5d78cbb6f7414a925a71a9b330818a/?a=1988&ch=0&cr=0&dr=0&lr=tiktok_m&cd=0%7C0%7C1%7C0&cv=1&br=1662&bt=831&cs=0&ds=3&ft=ebtHKH-qMyq8ZXRZ6we2NyEwfl7Gb&mime_type=video_mp4&qs=0&rc=NzplNjYzNTo5NDY8PDk0ZUBpanB1bzs6Zm48ZDMzNzczM0BhYV5hYDUyX14xLWMuM2JfYSNzX3BxcjRfZi9gLS1kMTZzcw%3D%3D&l=202207031807280100040040077350020310152F0E2"
-					channel="Channel2"
-					description="This is description2"
-					song="This is song2"
-					likes={111}
-					messages={222}
-					shares={333}
-				/>
+				{videos.map(({url, channel, description, song, likes, messages, shares}) => (
+					<Video
+						url={url}
+						channel={channel}
+						description={description}
+						song={song}
+						likes={likes}
+						messages={messages}
+						shares={shares}
+					/>
+				))};
 			</div>
 		</div>
 	);
--- tiktok/src/axios.js
+++ tiktok/src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create ({
-	baseURL:
+	baseURL: "https://dnguyen-tiktok-backend.herokuapp.com/"
 });
 
 export default instance;
